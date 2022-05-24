0b9776bd652545cb04a8e5f3722a89a118e34d08
João Malva <74738616+Malva2312@users.noreply.github.com>
2022-05-24 14:03:46 +0100

    progress

--- Sistemas-Operativos/cypher.c
+++ Sistemas-Operativos/cypher.c
@@ -5,18 +5,11 @@
 
 int main(int argc, char* argv[]){
 
-    char * line = NULL;
-    size_t len = 0;
-
-    char * ptoc[2]; //parent to children
-    char * ctop[2]; //children to parent
+    int ptoc[2]; //parent to children
+    int ctop[2]; //children to parent
 
     pid_t pid;
 
-    while(getline(&line, &len, stdin) > 0)
-    {
-        printf("%s", line);
-    }
 
     if (pipe(ptoc < 0))
     {
@@ -38,12 +31,39 @@ int main(int argc, char* argv[]){
     {
         close(ptoc[1]);
         close(ctop[0]);
+
+
+
     }
     else if (pid > 0) //parent process
     {
         close(ptoc[0]);
         close(ctop[1]);
+
+
+        char * str;
+        size_t len_str = 0;
+
+        fseek(stdin, 0, SEEK_END);
+        len_str = ftell(stdin);
+        fseek(stdin, 0, SEEK_SET);
+        str = malloc(sizeof(char) * len_str);
+        fread(str,1,len_str,stdin);
+
+
+
+        write(ptoc[1],str, len_str);
+        close(ptoc[1]);
+
+        if (waitpid(-1, NULL, 0) < 0)
+		{
+			perror("waitpid");
+			exit(EXIT_FAILURE);
+		}
+
+        read()
+        close(ctop[0])
     }
-    
+
 
 }
